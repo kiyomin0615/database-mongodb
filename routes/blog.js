98277e389629aadfbe5a2c166bd1437672089557
kiyomin0615 <moeday1010@gmail.com>
2023-03-21 23:21:56 +0900

    05. 데이터베이스 읽기3

--- routes/blog.js
+++ routes/blog.js
@@ -49,4 +49,20 @@ router.post("/posts", async function(req, res) {
   res.redirect("/posts");
 });
 
+router.get("/posts/:id", async function(req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new mongodb.ObjectId(postId) }, { summary: 0 });
+    // .project({ summary: 0 })
+
+  if (!post) {
+    res.status(404).render("404");
+    return;
+  }
+
+  res.render("post-detail", { post: post });
+})
+
 module.exports = router;
